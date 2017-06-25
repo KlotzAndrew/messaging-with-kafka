65810c0e8e489407396ee2c2ddfd8ab318d28790
Andrew Klotz <agc.klotz@gmail.com>
2017-06-25 13:44:52 -0400

    consumer and producer script

---
+++ message_consumer.rb
@@ -0,0 +1,18 @@
+require 'kafka'
+
+logger = Logger.new(STDOUT)
+
+kafka = Kafka.new(
+  # At least one of these nodes must be available:
+  # seed_brokers: ['kafka:9092'],
+  seed_brokers: ['http://192.168.99.100:9092'],
+
+  # Set an optional client id in order to identify the client to Kafka:
+  client_id: 'my-application',
+  logger: logger
+)
+# partitions = kafka.partitions_for('events')
+
+kafka.each_message(topic: 'greetings') do |message|
+  puts "Received: #{message.offset}, #{message.key}, #{message.value}"
+end
---
+++ message_producer.rb
@@ -0,0 +1,20 @@
+require 'kafka'
+
+logger = Logger.new(STDOUT)
+
+kafka = Kafka.new(
+  # At least one of these nodes must be available:
+  # seed_brokers: ['kafka:9092'],
+  seed_brokers: ['http://192.168.99.100:9092'],
+
+  # Set an optional client id in order to identify the client to Kafka:
+  client_id: 'my-application',
+  logger: logger
+)
+
+kafka.deliver_message('Hello, World!', topic: 'greetings')
+
+producer = kafka.producer(required_acks: :all)
+producer.produce('Async Hello!', topic: 'greetings')
+producer.deliver_messages
+producer.shutdown
